e4199feaacca51926a2f7e8f026dbc58f2697a7c
mindle714 <mindle714@naver.com>
2023-12-08 20:26:47 +0900

    normalized gaussian

--- modules/augments.py
+++ modules/augments.py
@@ -1,4 +1,10 @@
 import torch
 
 def add_gauss(x):
-    return x + torch.normal(mean=0., std=1., size=x.size()).to(x.device)
+    x_pow = (x**2).mean()
+    ns = torch.normal(mean=0., std=0.01, size=x.size()).to(x.device)
+    corr_x = x + ns
+
+    corr_x_pow = (corr_x**2).mean()
+    scale = torch.sqrt(x_pow / corr_x_pow)
+    return corr_x * scale
